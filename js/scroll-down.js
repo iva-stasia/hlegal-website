b39017046d37a1eea07af741b0e900a5cf0d488f
Anastasiia Ivanchenko <iva21sta@gmail.com>
2023-02-16 17:15:38 +0200

    change scroll button

--- js/scroll-down.js
+++
@@ -1,9 +0,0 @@
-let scrollBtn = document.querySelector('.banner__scroll-btn');
-scrollBtn.addEventListener('click', () => {
-    let tabsContainer = document.querySelector('.tabs');
-
-    window.scrollTo({
-        top: tabsContainer.offsetTop,
-        behavior: 'smooth',
-    });
-});
